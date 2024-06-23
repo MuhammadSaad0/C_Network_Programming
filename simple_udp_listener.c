8ed41f11f56d618adbc11be3447f45b18ff49677
MuhammadSaad0 <muhammadsaadz100@gmail.com>
2024-06-23 21:55:56 +0500

    add udp client server

---
+++ simple_udp_listener.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MYPORT "4950"
+#define MAXBUFLEN 100
+
+void *get_in_addr(struct sockaddr *sa) {
+    if(sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in *)sa)->sin_addr);
+    }
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+int main(void) {
+    int sockfd;
+    struct addrinfo hints, *serveinfo, *p;
+    int rv;
+    int numbytes;
+    struct sockaddr_storage their_addr;
+    char buf[MAXBUFLEN];
+    socklen_t addr_len;
+    char s[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype  = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if((rv = getaddrinfo(NULL, MYPORT, &hints, &serveinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo :%s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    for(p = serveinfo; p != NULL; p = p->ai_next) {
+        if((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("listener: socket");
+            continue;;
+        }
+        if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1){
+            close(sockfd);
+            perror("listener: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    if(p == NULL) {
+        fprintf(stderr, "listener: failed to bind socket");
+        return 2;
+    }
+    freeaddrinfo(serveinfo);
+
+    printf("listener: waiting to receive data...\n");
+
+    addr_len = sizeof their_addr;
+
+    if((numbytes = recvfrom(sockfd, buf, MAXBUFLEN - 1, 0, (struct sockaddr *)&their_addr, &addr_len)) == -1) {
+        perror("recvfrom");
+        exit(1);
+    }
+
+    printf("listener: got packet from %s\n", inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s));
+    printf("listener: packet is %d bytes long\n", numbytes);
+    printf("listener: packet contains '%s'\n", buf);
+
+    close(sockfd);
+
+    return 0;
+ }
